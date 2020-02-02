ec80c251425d71351704ca6d6d216b39a65c1c81
Nicoleta_Preda_Carp <nellyc007@yahoo.com>
2020-02-02 10:13:41 +0200

    Add Card Class with fields plus getter and setter.

---
+++ src/com/company/Card.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Card {
+    private String type;
+    private String value;
+    private int points;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}
