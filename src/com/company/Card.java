62a679aea28d217dfce208bce73ec9946f7a05a2
Nicoleta_Preda_Carp <nellyc007@yahoo.com>
2020-02-02 13:49:02 +0200

    Refactor suit field using enum Suits
    Add setSuit and getSuit.

--- src/com/company/Card.java
+++ src/com/company/Card.java
@@ -1,24 +1,46 @@
 package com.company;
 
 public class Card {
-    private String suit;
-    private String value;
+
+    private Suits suit;
+    private String rank;
     private int points;
 
-    public String getSuit() {
+    public enum Suits {
+        HEARTS,
+        DIAMONDS,
+        SPADES,
+        CLUBS,
+    }
+
+    public Suits getSuit() {
         return suit;
     }
 
-    public void setSuit(String suit) {
+    public void setSuit(Suits suit) {
         this.suit = suit;
     }
 
-    public String getValue() {
-        return value;
+
+
+
+//    public void determineCard () {
+//        String[] suit = {"Hearts", "Diamonds", "Spades", "Clubs" };
+//        String [] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+//        String[] card = new String[2];
+//        for (int i = 0; i<card.length; i++) {
+//            card[i] = rank[i] + suit[i];
+//            System.out.println(card[i]);
+//        }
+
+
+
+    public String getRank() {
+        return rank;
     }
 
-    public void setValue(String value) {
-        this.value = value;
+    public void setRank(String rank) {
+        this.rank = rank;
     }
 
     public int getPoints() {
